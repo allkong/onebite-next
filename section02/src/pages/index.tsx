a8469301ec5bc78df7c2fdcc704c5df73f57ce7e
Dabin Jeong <allempti@gmail.com>
2024-08-31 19:05:55 +0900

    ✅ Section 2.6: 스타일링

--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -1,7 +1,11 @@
+// CSS Module
+import style from './index.module.css';
+
 export default function Home() {
   return (
     <>
-      <h1>인덱스</h1>
+      <h1 className={style.h1}>인덱스</h1>
+      <h2 className={style.h2}>h2</h2>
     </>
   );
 }
