1f342ef7852ab5750ec2234646887e58e2ab6883
Dabin Jeong <allempti@gmail.com>
2024-08-31 19:23:46 +0900

    ✅ Section 2.7: 글로벌 레이아웃 설정

---
+++ section02/src/components/global-layout.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+import { ReactNode } from 'react';
+import style from './global-layout.module.css';
+
+export default function GlobalLayout({ children }: { children: ReactNode }) {
+  return (
+    <div className={style.container}>
+      <header className={style.header}>
+        <Link href='/'>📚 ONEBITE BOOKS</Link>
+      </header>
+      <main className={style.main}>{children}</main>
+      <footer className={style.footer}>제작 @allkong</footer>
+    </div>
+  );
+}
--- section02/src/pages/_app.tsx
+++ section02/src/pages/_app.tsx
@@ -1,38 +1,16 @@
+import GlobalLayout from '@/components/global-layout';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
 
 // 모든 페이지 역할을 하는 컴포넌트의 부모 컴포넌트
 // Component: 현재 페이지 역할을 하는 컴포넌트
 // pageProps: Component에 전달될 props
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-
-  const handleButtonClick = () => {
-    router.push('/test');
-  };
-
-  useEffect(() => {
-    router.prefetch('/test');
-  }, []);
-
   return (
     <>
-      <header>
-        <Link href='/'>index</Link>
-        &nbsp;
-        <Link href='/search' prefetch={false}>
-          search
-        </Link>
-        &nbsp;
-        <Link href='/book/1'>book/1</Link>
-        <div>
-          <button onClick={handleButtonClick}>/test 페이지로 이동</button>
-        </div>
-      </header>
-      <Component {...pageProps} />
+      <GlobalLayout>
+        <Component {...pageProps} />
+      </GlobalLayout>
     </>
   );
 }
