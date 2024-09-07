9734d275d09fb4b275d05ccbe52c46d313332893
Dabin Jeong <allempti@gmail.com>
2024-09-07 16:01:47 +0900

    ✅ Section 3.3: 레이아웃 설정

---
+++ section03/src/app/(with-searchbar)/layout.tsx
@@ -0,0 +1,10 @@
+import { ReactNode } from 'react';
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return (
+    <div>
+      <div>임시 서치바</div>
+      {children}
+    </div>
+  );
+}
