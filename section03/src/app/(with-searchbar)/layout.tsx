e549441d51ebd7735b575b547a1a507dd7e37813
Dabin Jeong <allempti@gmail.com>
2024-09-07 16:18:43 +0900

    ✅ Section 3.4: 리액트 서버 컴포넌트

--- section03/src/app/(with-searchbar)/layout.tsx
+++ section03/src/app/(with-searchbar)/layout.tsx
@@ -1,9 +1,10 @@
 import { ReactNode } from 'react';
+import Searchbar from './searchbar';
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div>
-      <div>임시 서치바</div>
+      <Searchbar />
       {children}
     </div>
   );
