e20cfac3a0dd78a1eee7229520dbc086f76b85e5
Dabin Jeong <allempti@gmail.com>
2024-09-08 18:03:43 +0900

    Section 5.2

---
+++ section05/src/app/(with-searchbar)/layout.tsx
@@ -0,0 +1,13 @@
+import { ReactNode, Suspense } from 'react';
+import Searchbar from '../../components/searchbar';
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return (
+    <div>
+      <Suspense fallback={<div>Loading ...</div>}>
+        <Searchbar />
+      </Suspense>
+      {children}
+    </div>
+  );
+}
